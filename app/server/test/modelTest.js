955eb7c9a5ff7d060b43580bfc2b50e251a69d49
omolewa <akinyomiomolewa30gmail.com>
2017-10-31 23:19:24 +0100

    write test

--- app/server/test/modelTest.js
+++ app/server/test/modelTest.js
@@ -1,4 +1,3 @@
-
 let Book = require('../models/book');
 
 let chai = require('chai');
@@ -42,7 +41,49 @@ it('it should not post if status is neither unavailable or available', (done) =>
             
       });
      });
-  });
+ 
+ it('it should not post book name is a number', (done) => {
+        let item = {
+            bookName: 1,
+            Author: "J.R.R. Tolkien",
+            bookStatus: "available"
+        };
+        chai.request(app)
+            .post('/api/v1/books')
+            .send(item)
+            .end((err, res) => {
+                res.should.have.status(500);
+              done();
+            
+      });
+     });
+  
+ 
+      it('it should put a book when all input supplied correctly', (done) => {
+        let item = {
+            bookName: "The Lord of the Rings",
+            Author: "J.R.R. Tolkien",
+            bookStatus: "available"
+        };
+        
+        chai.request(app)
+            .put('/api/v1/books/:bookId')
+            .send({'bookName': 'Spider'})
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('UPDATED');
+                res.body.UPDATED.should.be.a('object');
+                res.body.UPDATED.should.have.property('bookName, Author, bookStatus');
+                res.body.UPDATED.should.have.property('_bookId');
+                res.body.UPDATED.name.should.equal('item')
+              done();
+            
+      });
+     });
+    });
+  
   after(() => {
            process.exit(0)
          });
