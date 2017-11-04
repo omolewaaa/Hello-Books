1e100867481aeacd72a83dfefc7badefa9277d5d
omolewa <akinyomiomolewa30gmail.com>
2017-11-04 06:21:34 +0100

    Added API documentation using swagger

--- app/server/app.js
+++ app/server/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const swaggerJSDoc = require('swagger-jsdoc');
 // Set up the express app
 const app = express();
-//const Books = require('./models/book');
-//const path = require('path');
+
+const swaggerDefinition = {
+  info: {
+    
+    description: 'Welcome to Hello-Books',
+  },
+  //host: 'localhost:8000',
+  //basePath: '/',
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./app/server/route/index.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -14,11 +32,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('*', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 require('../server/route')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to Hello-Books.'}));
 
 module.exports = app;
-
-
