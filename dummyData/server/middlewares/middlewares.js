b46e0913321bdc5316cfd77d001f2eca905230fe
omolewa <akinyomiomolewa30gmail.com>
2017-11-19 16:58:37 +0100

    put dummy data in a separate folder

---
+++ dummyData/server/middlewares/middlewares.js
@@ -0,0 +1,27 @@
+const users = require('../user');
+const jwt = require('jsonwebtoken');
+const books = require('../book');
+
+module.exports = (req, res, next) => {
+ const token = (req.body['x-access-token'] ) || (req.headers['x-access-token'] ) || (req.query['x-access-token'] )
+  
+    //if (token) {
+     // jwt.verify(token, 'omolewa', (err, decoded)=> {
+       // if (err) {
+        //  return res.send(err);
+       // } 
+       // else {
+          req.decoded = decoded;
+          next();
+      
+         / }
+      
+       // });
+     // }
+   // else {
+      //return res.status(403).send({ 
+      // success: false, 
+       //message: 'No token provided.' 
+     // });
+    //}
+  }
