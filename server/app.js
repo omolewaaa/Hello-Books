f76a9fb2b390b0bdf22d00124144f942abbe9823
omolewa <akinyomiomolewa30gmail.com>
2017-11-19 21:08:04 +0100

    Document API using swagger

--- server/app.js
+++ server/app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 // Set up the express app
 const app = express();
+
+const swaggerDefinition = {
+  info: {
+    
+    description: 'Welcome to Hello-Books',
+  },
+  //host: 'localhost:3000',
+  //basePath: '/',
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./app/server/route/index.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 //const Books = require('./models/book');
 const path = require('path');
 
@@ -17,6 +39,11 @@ app.use(logger('dev'));
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 require('../server/route')(app);
