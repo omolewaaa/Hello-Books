820c901fa3df264147c9fde7c532f23ce8b95d2a
omolewa <akinyomiomolewa30gmail.com>
2017-11-19 14:24:52 +0100

    set up env for heroku deploy

--- bin/server.js
+++ bin/server.js
@@ -8,6 +8,7 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
-console.log('sever up and running' + port);
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
+})
