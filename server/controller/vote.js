9b870d0a4451f6d3db8c6dfdad7ad2b1e208b010
omolewa <akinyomiomolewa30gmail.com>
2017-11-26 05:47:31 +0100

    write method for voting

--- server/controller/vote.js
+++ server/controller/vote.js
@@ -33,14 +33,17 @@ exports.create = (req, res) => {
    })
    	.then((vote) =>{
    		if(vote){
+   			if (req.body.voteType === "upVotes" || req.body.voteType === "downVotes") {
    			if (vote.voteType === req.body.voteType){
    				res.status(400).send({message: 'You have already voted this book with this voteType' })
    			}
    			else{
+
    				vote.update({ voteType: req.body.voteType })
    				.then((vote) => {
 
    					//})
+
    				let count = 1
    				const up = books.upvotes = ++count;
   				if (req.body.voteType === "upVotes"){
@@ -48,24 +51,32 @@ exports.create = (req, res) => {
 
   					books.upvotes =++count 
   					//.then((books) => {
+
   					res.status(200).send({message: "You have upvoted successfuly"})
   				//})
   				}
-  				else{
+  				else if (req.body.voteType === "downVotes"){
   					books.downvotes =+ 1,
   					res.status(200).send({message: "You have downvoted this book successfuly"})
   				}
+  			//}
+  			
   			})
+   			}
+   			}
+   			else{
+   			return res.status(400).send({message: "You can either upvote or downvote"})
    			}
    		}
    		else{
+   			if (req.body.voteType === "upVotes" || req.body.voteType === "downVotes"){
    			votes.create({
    			book_id: req.params.bookId,
     		user_id: userId,
     		voteType: req.body.voteType
    			})
    			.then((votes) => {
-
+   				//if (req.body.voteType === "upVotes" || req.body.voteType === "downVotes"){
    				if (req.body.voteType === "upVotes"){
   					//books.upvotes.increment('number')
   					res.status(200).send({message: "You have upvoted successfuly", data: votes})
@@ -73,16 +84,26 @@ exports.create = (req, res) => {
   					
   				}
 
-  				else{
+  				else if (req.body.voteType === "downVotes"){
   					books.downvotes =+ 1,
   					res.status(200).send({message: "You have downvoted this book successfuly", data: votes})
   				}
+  				//}
    			})
    			.catch(error => res.status(400).send(error));
    		}
+   		else{
+   			return res.status(400).send({message: "You can either upvote or downvote"})
+   		}
+   		}
+   		
+   //	}
+
+
    	})
 	//}
   // })
 }
+
 })
 }
